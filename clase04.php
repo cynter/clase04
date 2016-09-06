d45656383404a127212957ad8081ee67d791f3f0
cynter <cynteran27@gmail.com>
2016-09-06 20:04:15 -0300

    estilos

---
+++ clase04.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/estilo.css" >
+	<link rel="stylesheet" type="text/css" href="css/animacion.css">
+
+
+</head>
+
+<body  style="background: <?php  echo $_GET['color'];?>">
+	<div class="CAjaInicio" class="Animated.bounce ">
+		<form  id="FormIngreso">
+			<input type="text" name="color">
+			<input type="submit" class="MiBotonUTNMenuInicio" >
+
+		</form>
+	</div>
+		
+
+</body>
+</html>
